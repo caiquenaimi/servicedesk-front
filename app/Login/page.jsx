8a082a1907ca8fe33cbac7a0ca6a71101c84e2b2
Caique <caiquenaimi.564@gmail.com>
2024-12-03 16:37:59 -0300

    Adicionar comentário no componente Login

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -4,6 +4,7 @@ import {Header} from '../components/Header/Header.jsx';
 export default function Login() {
     return (
         <main>
+            {/* feliz */}
             <Header/> 
             <UserComponent/> 
         </main>
